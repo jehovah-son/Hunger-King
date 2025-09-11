71036fc18ee928793c9c6f53b0988486b4ef624b
jehovah-son <chukwukasuccess0@gmail.com>
2025-09-11 21:01:10 +0100

    `Refactor MenuPage component: updated class names and styles`

--- src/app/menu/page.tsx
+++ src/app/menu/page.tsx
@@ -6,12 +6,12 @@ import clsx from "clsx";
 
 export default function MenuPage() {
   return (
-    <div className="p-4 lg:px-20 xl:px-40 flex flex-col md:flex-row gap-6">
+    <div className="p-4 lg:px-20 xl:px-30 flex flex-col md:flex-row gap-6">
       {menu.map((item) => (
         <Link
           href={`/menu/${item.slug}`}
           key={item.id}
-          className="relative md:h-96 h-50 rounded-lg overflow-hidden group"
+          className="relative md:h-96 h-50 rounded-lg overflow-hidden w-full group"
           style={{
             backgroundImage: `url(${item.img})`,
             backgroundSize: "cover",
@@ -23,12 +23,12 @@ export default function MenuPage() {
 
           {/* Text content */}
           <div
-            className={` flex flex-1 flex-col justify-center items-start p-6  h-full w-full text-${item.color}`}
+            className={` flex flex-1 flex-col justify-center items-start p-8  h-full w-1/2 text-${item.color} `}
           >
             <h2 className="text-2xl md:text-4xl font-bold mb-2">
               {item.title}
             </h2>
-            <p className=" text-xs font-bold ">{item.desc}</p>
+            <p className=" text-xs ">{item.desc}</p>
             {/* <button
               className={`mt-4  text-white px-6 py-2 rounded-md hover:bg-${
                 item.color
