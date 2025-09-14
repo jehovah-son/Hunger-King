aac50b39cf5f5b26fd2af04caf31ec4f8e47e725
jehovah-son <chukwukasuccess0@gmail.com>
2025-09-14 21:31:07 +0100

    `Added client-side rendering, state management and quantity control to Price component`

--- src/component/Price.tsx
+++ src/component/Price.tsx
@@ -1,20 +1,33 @@
+"use client";
 import Button from "./Button";
+import { useState, useEffect } from "react";
 type Props = {
   price: number;
   options: { title: string; additionalPrice: number }[] | undefined;
 };
 
 export default function Price({ price, options }: Props) {
+  const [selected, setSelected] = useState(0);
+  const [totalPrice, setTotalPrice] = useState(price);
+  const [quantity, setQuantity] = useState(1);
   return (
     <div className="flex flex-col gap-5">
       {/* price */}
-      <h1 className="">${price.toFixed(2)}</h1>
+      <h1 className="">${totalPrice.toFixed(2)}</h1>
       {/* size */}
       <div className="flex gap-5 ">
         {options?.map((option, index) => (
           <button
             className="ring-1 ring-red-500  min-w-[6rem] p-2 rounded-md"
             key={index}
+            style={{
+              background: selected === index ? "rgb(248 113 113)" : "white",
+              color: selected === index ? "white" : "rgb(248 113 113)",
+            }}
+            onClick={() => {
+              setSelected(index);
+              setTotalPrice(price + option.additionalPrice);
+            }}
           >
             {option.title}
           </button>
@@ -28,9 +41,17 @@ export default function Price({ price, options }: Props) {
 
           {/* Quantity */}
           <div className="flex justify-center item-center gap-4">
-            <button>{"<"}</button>
-            <span>1</span>
-            <button>{">"}</button>
+            <button
+              onClick={() => setQuantity((prev) => (prev > 1 ? prev - 1 : 1))}
+            >
+              {"<"}
+            </button>
+            <span>{quantity}</span>
+            <button
+              onClick={() => setQuantity((prev) => (prev < 9 ? prev + 1 : 9))}
+            >
+              {">"}
+            </button>
           </div>
         </div>
         {/* button */}
